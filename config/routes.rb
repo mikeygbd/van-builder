962456374ca228adacb194e71d0dd1f3626d6f95
mikeygbd <mikey.soares@gmail.com>
2019-04-10 23:58:17 -0400

    added custom routes for destroy to be able to delete parts and posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :wishlist_parts
   resources :sessions
   resources :users
-
+  get '/parts/:id/delete', to: 'parts#destroy'
+  get '/wishlist_parts/:id/delete', to: 'wishlist_parts#destroy'
+  get '/posts/:id/delete', to: 'posts#destroy'
   # Routes for Google authentication
   devise_scope :user do
   get "/users/auth/google_oauth2/callback", to: "users/omniauth_callbacks#google"
