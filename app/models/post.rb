9cda00d52c5959ed0756fbb6d3d7045c1f3e7146
mikeygbd <mikey.soares@gmail.com>
2019-04-13 23:08:16 -0400

    Added border radiuses on headers of all page and totals of parts and wishlist_parts. Also designed the comments header in a similar way.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+  scope :published, -> { where(published: true) }
+ scope :published_and_commented, -> { published.where("comments_count > 0") }
 
   validates :title, presence: true
   validates :description, presence: true
