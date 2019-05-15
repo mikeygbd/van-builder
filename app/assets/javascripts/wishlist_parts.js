42ac91e54e4ef8c198f3d22327e28052c312ab12
mikeygbd <mikey.soares@gmail.com>
2019-05-14 23:51:23 -0400

    took the same form foreach and ajax request and implented the correct html for the parts and wishlist_parts

--- app/assets/javascripts/wishlist_parts.js
+++ app/assets/javascripts/wishlist_parts.js
@@ -1,1 +1,57 @@
 
+$(document).on('turbolinks:load', function () {
+  $("form#new_wishlist_part").toArray().forEach(f => {
+    $(f).on("submit", function(e){
+      e.preventDefault()
+
+      url = this.action
+
+      data = {
+        'authenticity_token': f.authenticity_token.value,
+        'wishlist_part': {
+          'name': f['wishlist_part[name]'].value,
+          'price': f['wishlist_part[price]'].value,
+          'description': f['wishlist_part[description]'].value,
+          'manufacturer': f['wishlist_part[manufacturer]'].value,
+          'qty': f['wishlist_part[qty]'].value,
+
+        }
+      }
+      $('.modal').removeClass('in');
+                  $('.modal').attr("aria-hidden","true");
+                  $('.modal').css("display", "none");
+                  $('.modal-backdrop').remove();
+                  $('body').removeClass('modal-open');
+
+
+      $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    success: function(response){
+
+      const wishlistParts = $(`.wishlistparts`)
+      const homeWishlistParts = $('.homeWishlistParts')
+      let fullWishlistPart = `<form class="wlform shadow"><div id="contact">
+        <h2><strong>${response.manufacturer} ${response.name}</strong> </h2>
+        <h4>${response.description}<br></h4>
+        <h2><strong>Quantity:</strong> ${response.qty}<br></h2>
+        <img src="${response.url}"></img><br>
+        <h2><strong>Unit Price: </strong>$${response.price}</h2>
+        <h2><strong>Total Price: </strong>$${response.price * response.qty}</h2><br>
+        <a href="/parts/${response.id}/edit">Edit</a>
+        &nbsp; | &nbsp;
+        <a href="/parts/${response.id}/delete">Delete</a>
+      </div></form>`
+
+  let fullHomeWishlistPart = `<div class="list-group shadow">
+  <a href= '/parts/${response.id}' role="button" class="list-group-item"><img width=50 src= "${response.url}"> ${response.manufacturer} ${response.name}<br> <strong>Qty:</strong> ${response.qty} </a>
+    </div></div>`
+      wishlistParts.append(fullWishlistPart)
+      homeWishlistParts.append(fullHomeWishlistPart)
+    }
+  });
+f.reset()
+})
+  })
+})
