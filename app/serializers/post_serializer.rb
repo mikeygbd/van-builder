b45d15608a38b975c8eae0795fae5cde58c411c1
mikeygbd <mikey.soares@gmail.com>
2019-05-16 09:10:16 -0400

    added belongs to a user to post_serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,18 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :embed, :user_id
   has_many :comments
-  has_one :user
+  belongs_to :user
 
-  # def comment_list
-  # object.comments.map do |comment|
-  #   {
-  #     id: comment.id,
-  #     user: {
-  #       id: comment.user_id,
-  #       email: User.find(comment.user_id).email
-  #     },
-  #     content: comment.content
-  #   }
-  #   end
-  # end
+
 end
